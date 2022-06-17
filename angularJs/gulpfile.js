84e4f6094e22254746a1b02776353646c81288dd
gaetanV <gaetan.vigneron@gmail.com>
2022-06-17 10:23:13 +0200

    [FEATURE] update angular js to 1.8.3

--- angularJs/gulpfile.js
+++ angularJs/gulpfile.js
@@ -3,22 +3,22 @@ var ts = require('gulp-typescript');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
-gulp.task('ts', function () {
+const tsTask =  function () {
     return gulp.src([
         'app/service/rest.module.ts',
         'app/factory/factory.module.ts', 
         'angular/**/*.ts',
         'app/**/*.ts'
     ])
-           .pipe(ts({
-                noImplicitAny: true,
-                outFile: 'boot.js',
-                experimentalDecorators: true,
-            }))
-            .pipe(gulp.dest('app/'));
-});
+    .pipe(ts({
+        noImplicitAny: true,
+        outFile: 'boot.js',
+        experimentalDecorators: true,
+    }))
+    .pipe(gulp.dest('app/'));
+}
 
-gulp.task('directive', function () {
+const directiveTask = function () {
     return gulp.src([
         'directive/gaetan.module.js',
         'directive/**/*.js'
@@ -26,16 +26,25 @@ gulp.task('directive', function () {
        .pipe(concat('directive.js'))
        .pipe(uglify())
        .pipe(gulp.dest('app/'));
-});
+};
 
-gulp.task('css', function () {
+const cssTask = function () {
     return gulp.src(['directive/**/*.css'])
-       .pipe(concat('directive.css'))
-       .pipe(gulp.dest('content/'));
-});
+    .pipe(concat('directive.css'))
+    .pipe(gulp.dest('content/'));
+};
 
-gulp.task('watch', ['ts','directive','css'], function () {
-    gulp.watch('app/**/*.ts', ['ts']);
-    gulp.watch('angular/**/*.ts', ['ts']);
-    gulp.watch('directive/*.js', ['directive']);
-});
+const watchTask =  function() {
+    gulp.watch('app/**/*.ts', tsTask);
+    gulp.watch('angular/**/*.ts', tsTask);
+    gulp.watch('directive/**/*.js', directiveTask);
+    gulp.watch('directive/**/*.css', cssTask);
+}
+
+gulp.task('ts', tsTask);
+gulp.task('directive', directiveTask);
+gulp.task('css', cssTask);
+gulp.task('watch', gulp.series(tsTask,directiveTask,cssTask,watchTask));
+
+exports.default =  gulp.series(tsTask,directiveTask,cssTask,watchTask);
+
